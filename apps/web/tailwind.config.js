2459ce2d0eb882ab8947dbd3f43f3def817920cb
JoelVR17 <vermudo.com@gmail.com>
2025-07-23 17:16:31 -0600

    refactor: standardize formatting in tailwind.config.js for consistency

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -31,10 +31,7 @@ module.exports = {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
         },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
+        secondary: '#0E1827',
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
